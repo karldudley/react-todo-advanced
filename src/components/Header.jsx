3919da1bc446aba3d9ae10454291d1ce82be4ba5
Karl Dudley <karldudley@gmail.com>
2025-09-03 07:23:16 +0100

    Fix header bug

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Header(props) {
     const { todos } = props;
-    const todosLength = todos.length;
+    const todosLength = todos.filter(todo => !todo.complete).length;
     const taskOrTasks = todosLength === 1 ? 'task' : 'tasks';
 
     return (
